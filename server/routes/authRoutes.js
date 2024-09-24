d66c15b1206ddd162dd7034232fb0d6af19e6944
Hariharan <hariharimass1205@gmail.com>
2024-09-24 17:36:13 +0530

    3-04-05

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,8 @@
 import {Router} from 'express'
 import { login, signUp, uploadUrl, verifyjwt } from '../controller/authController.js'
-import {checkJwt} from '../middleware/checkJwt.js'
 const authRoutes=Router()
 authRoutes.post('/signup',signUp)
-authRoutes.get('/verifyjwt',checkJwt,verifyjwt)
+authRoutes.get('/verifyjwt',verifyjwt)
 authRoutes.post('/login',login)
 authRoutes.post('/uploadurl',uploadUrl)
 
